15770038ddf5bd8502712bb4e7f29244c7cacf28
RamyaSabapathi2018 <mailramya26@gmail.com>
2020-09-07 14:58:36 +0530

    Added to Sample1 branch

--- src/test/java/org/AddAll/PushFileOne.java
+++ src/test/java/org/AddAll/PushFileOne.java
@@ -3,6 +3,15 @@ package org.AddAll;
 public class PushFileOne {
 	public static void main(String[] args) {
 		System.out.println("Adding PushFileOne file");
+		int a=10;
+		int b=20;
+		System.out.println("Before Swap a value---->"+ a);
+		System.out.println("Before Swap b value---->"+ b);
+		a = a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("After Swap a value---->"+ a);
+		System.out.println("After Swap b value---->"+ b);
 	}
 
 }
