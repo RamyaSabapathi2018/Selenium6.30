01ca34735130d4e32c995aa9f301a29b66cc79ed
RamyaSabapathi2018 <mailramya26@gmail.com>
2020-09-07 16:50:21 +0530

    To check for Conflict check

--- src/test/java/org/AddAll/PushFileOne.java
+++ src/test/java/org/AddAll/PushFileOne.java
@@ -3,17 +3,7 @@ package org.AddAll;
 public class PushFileOne {
 	public static void main(String[] args) {
 		System.out.println("Conflict Check");
-		System.out.println("Adding PushFileOne file");
-		int a=10;
-		int b=20;
-		System.out.println("Before Swap a value---->"+ a);
-		System.out.println("Before Swap b value---->"+ b);
-		a = a+b;
-		b=a-b;
-		a=a-b;
-		System.out.println("After Swap a value---->"+ a);
-		System.out.println("After Swap b value---->"+ b);
-		System.out.println("Adding to merge withou conflict");
+		System.out.println("removed piece of code");
 		int c=30;
 		int d= 45;
 		int temp=0;
